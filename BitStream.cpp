e9657d7300eed4cb6a9f50b6a11ecc6bccb34576
Bernardo Barreto <bernardo.barreto@ua.pt>
2021-11-29 15:46:05 +0000

    Write & read 1 Bit methods

--- BitStream.cpp
+++
@@ -1,4 +0,0 @@
-class BitStream{
-    private:
-    
-};
---
+++ BitStream.hh
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class BitStream{
+    public:
+        BitStream(string inFile="", string outFile="");
+        void writeBit(int bit);
+        int readBit(void);
+    private:
+        vector<int> buffer;
+        fstream inFile;
+        fstream outFile;
+};
+
+BitStream::BitStream(string inF, string outF){
+    if(not inF.empty()) {
+        inFile.open(inF);
+        char c;
+        while (inFile.get(c)) {
+            for (int i = 7; i >= 0; i--) // or (int i = 0; i < 8; i++)  if you want reverse bit order in bytes
+                buffer.push_back(((c >> i) & 1));
+        }
+    }else{
+        cout << "No In File to open" << endl;
+    }
+
+    if(not outF.empty()){
+        outFile.open(outF);
+    }else{
+        cout << "No out File to open" << endl;
+    }
+}
+
+void BitStream::writeBit(int bit){
+    if(outFile.is_open()){
+        outFile << bit;
+    }else{
+        cout << "File not Open!!" << endl;
+    }
+}
+
+int BitStream::readBit(){
+    if(inFile.is_open()){
+        vector<int> aux;
+        int bit=buffer.at(0);
+        for(int i=1 ; i<buffer.size() ; i++){
+            aux.push_back(buffer.at(i));
+        }
+        buffer=aux;
+        return bit;
+    }
+    cout << "File not Open!!" << endl;
+    return 1;
+}
+
+// 10110001 00000001 
