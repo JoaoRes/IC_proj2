a014e63b4905c57a0685c7e72d037ff517c588f6
Joao Oliveira <joaoroliveira@ua.pt>
2021-12-19 17:04:52 +0000

    golomb

--- Golomb.hh
+++ Golomb.hh
@@ -1,49 +1,97 @@
 #include <iostream>
 #include <math.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Golomb{
     public:
         Golomb(int m);
         void encoder(int num);
-        void decoder();
+        void decoder(string code, int m);
         void decToBinary(int n);
     private:
         int m_;
+        int q=0;
+        int r=0;
 };
 
-Golomb::Golomb(int m=0){
-    if(m=0){
-        //calculate m;
-    }
+Golomb::Golomb(int m){
     m_=m;
 }
 
 void Golomb::encoder(int num){
-    int q=0;
-    int r=0;
+    bitset<64> r_binary;
+    uint64_t numUnary = 0;
+    string numBinary;
 
     q=floor(num/m_);
     r=num-q*m_;
 
+    if(q == 0) {
+        numUnary = 0;
+    }
+    else if (q>=1) {
+        numUnary=1;
+        for(int i = 0 ; i< q; i++) {
+            numUnary= (numUnary*10)+1;
+        }
+        numUnary=numUnary-1;
+    }
+
+    // if m is power of 2
+    if(ceil(log2(m_)) == floor(log2(m_))){
+        int firstONE = 0;
+        string auxStr = bitset<64>(r).to_string();
+        for (int i=0; i<64; i++){
+            if(auxStr[i]=='1' && firstONE==0){
+                firstONE=1;
+                numBinary+=auxStr[i];
+            }
+            else if (firstONE==1) {
+                numBinary+=auxStr[i];
+            }   
+        }
+    } else { // if m is not power of 2
+        int b=ceil(log2(m_));
+        int nBits;
+
+        if(r<b){
+            nBits=b-1;
+        }
+        else {
+            r = r+pow(2, b);
+            nBits = b;
+        }
+
+        int aux=0;
+        string auxStr = bitset<64>(r).to_string();
+        string reverse;
+
+        for (int j=63; j>63-nBits; j--){
+            reverse=auxStr[j]+reverse;
+        }
+
+        numBinary+=reverse;
+    }
+    
+    cout << "unary " << numUnary  << " binary " << numBinary << endl;
 
 }
 
-// void decToBinary(int n) {
-//     // array to store binary number
-//     int binaryNum[1000];
-  
-//     // counter for binary array
-//     int i = 0;
-//     while (n > 0) {
-  
-//         // storing remainder in binary array
-//         binaryNum[i] = n % 2;
-//         n = n / 2;
-//         i++;
-//     }
-  
-//     // printing binary array in reverse order
-//     for (int j = i - 1; j >= 0; j--)
-//         cout << binaryNum[j];
-// }
+void Golomb::decoder(string code, int m){
+    int separation = (int) code.find('0');
+
+    string unary_q = code.substr(0,separation);
+    string binary_r = code.substr(separation+1);
+
+    int q = sizeof(unary_q)/sizeof(char);
+
+    cout << "q " << q << endl;
+
+    cout << "unary " << unary_q << endl;
+
+    cout << "binary " << binary_r << endl;
+
+    
+    
+}
